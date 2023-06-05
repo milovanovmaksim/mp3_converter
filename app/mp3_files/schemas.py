77677ee29784ff8b2c88c655358a675a6cd5694b
milovanovmaksim <milovanov160386@gmail.com>
2023-06-05 17:47:17 +0300

    added some docstring

--- app/mp3_files/schemas.py
+++ app/mp3_files/schemas.py
@@ -6,6 +6,9 @@ from app.web.schemes import OkResponseSchema
 class Mp3FileShcemaRequest(Schema):
     """
     Класс представляет заголовки POST-запроса для конечной точки /files.convert.
+    Args:
+        user_id: идентификатор пользователя.
+        user_uuid: uuid-токен пользователя.
     """
     user_id = fields.Int(required=True, allow_none=False)
     user_uuid = fields.Str(required=True, allow_none=False,
@@ -15,6 +18,8 @@ class Mp3FileShcemaRequest(Schema):
 class Mp3FileShcemaResponse(OkResponseSchema):
     """
     Класс представляет ответ на POST-запроса для конечной точки /files.convert.
+    Args:
+        url: url-адрес для скачивания конвертированного файла в формате mp3.
     """
     url = fields.Str()
 
@@ -23,6 +28,9 @@ class RequestMp3DownloadFileSchema(Schema):
     """
     Класс представляет параметры url адреса
     /files.record?record_id=id_записи&user_id=id_пользователя GET-запроса.
+    Args:
+        record_id: идентификатор mp3 файла.
+        user_id: идентификатор пользователя.
 
     """
     user_id = fields.Int(required=True, allow_none=False)
